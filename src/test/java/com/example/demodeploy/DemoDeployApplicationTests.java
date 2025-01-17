60347abb34e2d630433979de42b795fda7285999
DBJ247 <gkalfkd@gmail.com>
2025-01-17 15:51:41 +0900

    HotFix: gitaction 단위 테스트 오류 해결

--- src/test/java/com/example/demodeploy/DemoDeployApplicationTests.java
+++ src/test/java/com/example/demodeploy/DemoDeployApplicationTests.java
@@ -10,9 +10,9 @@ class DemoDeployApplicationTests {
     void contextLoads() {
         // 단위 테스트
         // 에러 강제 발생 시킴 -> 중단여부 체크
-        // 테스트 실패되면 반영 x
-        int a = 1/0;
-        throw new RuntimeException("오류발생");
+        // 테스트 실패되면 반영 x(깃에는 반영, 배포 및 운영에서는 반영x)
+        //int a = 1/0;
+        //throw new RuntimeException("오류발생");
         // MVC 테스트 -> 목업통해서 가상 처리 가능
         // RestAPI 테스트 가능
         // 메시지가 보이지는 않지만, 차후 코드로 확인
